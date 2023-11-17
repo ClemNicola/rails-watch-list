a1d82a87bb3d309a5f2441900e841630375c1ecc
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-17 11:36:42 +0100

    new root

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "simple_form"
 
 gem "sassc-rails"
 
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
 
